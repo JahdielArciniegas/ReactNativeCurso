e8fb1b1b1135209dc12c8a3e35ae00da0c83b487
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-06-06 11:54:43 -0400

    Haciendo temas en un archivo aparte y pasando datos a otra ruta

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,48 @@
+import { darkTheme, lightTheme } from "@/styles/theme";
 import { useRouter } from "expo-router";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button, Text, View } from "react-native";
 // eslint-disable-next-line import/no-named-as-default
-import styled from "styled-components/native";
+import styled, { ThemeProvider } from "styled-components/native";
+import { useState } from "react";
 
 export default function Screen1() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
   const router = useRouter();
 
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+  const currentTheme = isDarkTheme ? darkTheme : lightTheme;
+  const data = {
+    nombre: "Frank",
+    curso: "React Native",
+  };
+
   return (
-    <View>
-      <Title>hola desde Screen 1</Title>
-      <Button title="Ir a screen 2" onPress={() => router.push("/screen2")} />
-    </View>
+    <ThemeProvider theme={currentTheme}>
+      <Container>
+        <Title>hola desde Screen 1</Title>
+        <Button
+          title="Ir a screen 2"
+          onPress={() =>
+            router.push({
+              pathname: "/screen2",
+              params: data,
+            })
+          }
+        />
+        <Button title="Cambiar tema" onPress={toggleTheme} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
 const Title = styled.Text`
-  color: #82f13c;
+  color: ${({ theme }) => theme.colors.secondary};
   font-size: 42px;
+`;
+const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
 `;
---
+++ styles/theme.tsx
@@ -0,0 +1,27 @@
+export const lightTheme = {
+  colors: {
+    primary: "#82f13c",
+    secondary: "#ec6c6c",
+    background: "#fff",
+    text: "#000",
+  },
+  fonts: {
+    large: "20px",
+    medium: "16px",
+    small: "12px",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    primary: "#151718",
+    secondary: "#82f13c",
+    background: "#151718",
+    text: "#fff",
+  },
+  fonts: {
+    large: "20px",
+    medium: "16px",
+    small: "12px",
+  },
+};
