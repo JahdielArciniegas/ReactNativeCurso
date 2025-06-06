f93375b99a517dbe8a352f7f78da12d579ef6b4f
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-06-06 12:13:06 -0400

    Utilizando el TextInput y el ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,15 @@
 import { darkTheme, lightTheme } from "@/styles/theme";
 import { useRouter } from "expo-router";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Button, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  TextInput,
+} from "react-native";
 // eslint-disable-next-line import/no-named-as-default
 import styled, { ThemeProvider } from "styled-components/native";
 import { useState } from "react";
@@ -9,23 +17,20 @@ import { useState } from "react";
 export default function Screen1() {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
 
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
   const currentTheme = isDarkTheme ? darkTheme : lightTheme;
 
   return (
     <ThemeProvider theme={currentTheme}>
       <Container>
-        <ImageContainer source={require("../assets/images/icon.png")} />
+        {/* <Image source={require("../assets/images/splash-icon.png")} /> */}
+        <TextInputEntrada keyboardType="phone-pad" />
       </Container>
     </ThemeProvider>
   );
 }
 
-const ImageContainer = styled.Image`
-  width: 100px;
-  height: 100px;
+const TextInputEntrada = styled.TextInput`
+  color: #fff;
 `;
 
 const Container = styled.View`
